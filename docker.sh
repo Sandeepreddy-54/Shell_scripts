1d59332887a1239889b0a1fea5a6855759ab3da7
Sandeepreddy-54 <kskreddy54@gmail.com>
2023-12-29 17:30:45 +0530

    adding new functions

--- docker.sh
+++ docker.sh
@@ -5,45 +5,56 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOG=docker-install.log
-USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ]; then
-	echo  -e "$R You are not the root user, you dont have permissions to run this $N"
-	exit 1
-fi
 
-validate(){
-    if [$1 -ne 0]; then 
-        echo -e "$2 ... $R Failed $N"
-        exit 1
-    else
-        echo -e "$2... $G Success $N"
-    fi
+PACKAGES=("git" "docker") 
+
+LOG=docker-install.log
+
+is_package_installed(){
+    rpm -q "$1" &>> $LOG
 }
 
-## Git Insatll 
+install_package(){
+    if ! is_package_installed "$1"; then 
+        echo "$Y Installing $1... $N"
+        sudo apt insatll -y "$1"
+        if [ $? -eq 0 ]; then 
+             echo "$G $1 installed succesfully.... $N"
+        else 
+            echo "$R Error installing in $1... $N"
+    else 
+        echo "$G $1 is installed alredy.... $N"
+}
 
-sudo yum install git 
-validate $? "Git installed"
+for package in "${PACKAGES[@]}"; do 
+    install_package "$package"
+done 
 
-## Docker install 
-sudo yum install docker 
-validate $? "docker installed"
+echo "All packages and dependencies installed successfully."
+
+VALIDATE(){
+	if [ $1 -ne 0 ]; then
+		echo -e "$2 ... $R FAILED $N"
+		exit 1
+	else
+		echo -e "$2 ... $G SUCCESS $N"
+	fi
+}
 
 ## docker demon start 
-sudo systemctl start docker
+sudo systemctl start docker &>> $LOG
 validate $? "docker demon start"
 
 ## give persmissions to doker.sock 
-sudo chown root:docker /var/run/docker.sock
+sudo chown root:docker /var/run/docker.sock &>> $LOG
 validate $? "chown to docker.sock provide"
 
 ## Add user to docker group
-sudo usermod -aG docker $USER
+sudo usermod -aG docker $USER &>> $LOG
 validate $? "User added to docker group"
 
 ## Re-start docker 
-sudo systemctl restart docker
+sudo systemctl restart docker &>> $LOG
 validate $? "docker restart"
 
 ## sudowm 
